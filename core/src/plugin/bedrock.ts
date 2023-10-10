0f652fec40ef442d2e8c7c1393765e82e80af579
Corey Pyle <corepyle@amazon.com>
2023-10-10 11:14:11 -0400

    Implement Bedrock/Claude integration.

--- core/src/plugin/bedrock.ts
+++ core/src/plugin/bedrock.ts
@@ -1,32 +1,35 @@
 import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime';
-import { get_encoding } from "@dqbd/tiktoken";
 import { Content } from "../content.js";
 import { isDefined } from "../util.js";
 import { Message, Summary } from "./index.js";
 
-const MODEL = 'anthropic.claude-v2';
+export const DEFAULT_MODEL = 'anthropic.claude-v2';
 
 export function buildClaude2Prompt(messages: Message[]) {
-  const prefixedMessages = messages.map(m => {
-    let prefix: "Human" | "Assistant";
-    if (m.role === "user" || m.role === "system") {
-      prefix = "Human";
-    } else {
-      prefix = "Assistant";
+  let output = '';
+  let prevRole = "";
+
+  const roleAdjustedMsgs = messages.map(msg => ({ ...msg, role: msg.role === 'assistant' ? 'Assistant' : 'Human' }))
+
+  roleAdjustedMsgs.forEach(msg => {
+    if (msg.role !== prevRole) {
+      output += '\n\n' + msg.role + ': ';
+      prevRole = msg.role;
+    }
+    else {
+      output += '\n';
     }
 
-    return `\n\n${prefix}: ${m.content}`
+    output += msg.content;
   });
 
-  prefixedMessages.push("\n\nAssistant:");
-
-  return prefixedMessages.join("");
+  return output + '\n\nAssistant:';
 }
 
 export async function generate(
   c: Content,
   {
-    model = MODEL,
+    model = DEFAULT_MODEL,
   }: { model: string; }
 ): Promise<{ message: string; debug: unknown }> {
   const bedrock = new BedrockRuntimeClient({});
@@ -49,7 +52,7 @@ export async function generate(
     modelId: model,
     contentType: 'application/json',
     accept: 'application/json',
-    body: buildClaude2Prompt(messages)
+    body: JSON.stringify({ prompt: buildClaude2Prompt(messages), max_tokens_to_sample: 8191 })
   }));
 
   const body = JSON.parse(response.body.transformToString());
@@ -67,26 +70,19 @@ export async function generate(
     },
   };
 }
-// TODO: update formatter here for bedrock
+
 export async function format(contents: Content[]): Promise<Summary> {
   const summary: Summary = { prompts: contents.length, tokens: 0 };
   for (const content of contents) {
-    summary.tokens += await addContentMeta(content);
+    await addContentMeta(content);
   }
   return summary;
 }
 
-const encoding = get_encoding("cl100k_base");
 async function addContentMeta(content: Content) {
   content.meta ??= {};
   content.meta.messages = getMessages(content);
   content.meta.tokens = 0;
-  for (const message of content.meta.messages) {
-    const toks = (await encoding.encode(message.content)).length;
-    message.tokens = toks;
-    content.meta.tokens += toks;
-  }
-  return content.meta.tokens;
 }
 
 export function getMessages(content: Content): Message[] {
