7779b6e47c1ccd0b61369b16da3c1c83f0f2c81a
David Souther <davidsouther+github@gmail.com>
2024-04-22 11:53:44 -0400

    Udpate tests

--- integ/integ.sh
+++ integ/integ.sh
@@ -14,13 +14,13 @@ export AILLY_ENGINE=${AILLY_ENGINE:-noop}
 
 echo "basic"
 npx ailly --root 01_basic
-[ -f 01_basic/basic.ailly.md ]
-rm 01_basic/basic.ailly.md
+[ -f 01_basic/basic.txt.ailly.md ]
+rm 01_basic/basic.txt.ailly.md
 
 echo "combined"
 npx ailly --root 02_combined --combined
-[ ! -f 02_combined/combined.ailly.md ]
-git restore 02_combined/combined
+[ ! -f 02_combined/combined.txt.ailly.md ]
+git restore 02_combined/combined.txt
 
 echo "edit"
 AILLY_NOOP_RESPONSE="Edited" npx ailly --root 04_edit --edit file --lines 2:4 --prompt "edit" --yes
