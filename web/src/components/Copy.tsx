58086945d4793cc8ad9008519fab1e26a8cbde04
David Souther <davidsouther@gmail.com>
2024-05-08 21:00:53 -0400

    Add copy buttons to prompt and response outputs (#96)

---
+++ web/src/components/Copy.tsx
@@ -0,0 +1,14 @@
+import { useCallback } from "react";
+import styles from "./Copy.module.css";
+
+export const Copy = (props: { contents: string }) => {
+  const copy = useCallback(() => {
+    window.navigator.clipboard.writeText(props.contents);
+  }, [props.contents]);
+
+  return (
+    <button className={styles.button} onClick={copy}>
+      <span className="material-symbols-outlined">content_copy</span>
+    </button>
+  );
+};
