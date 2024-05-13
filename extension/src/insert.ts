da9d2229ead30679c6bf6bbc77c6415dfb6226a7
David Souther <davidsouther+github@gmail.com>
2024-05-13 09:28:26 -0400

    Ailly will stream edits into vscode

---
+++ extension/src/insert.ts
@@ -0,0 +1,16 @@
+import vscode from "vscode";
+import { AillyEdit } from "@ailly/core/src/content/content";
+import { isAillyEditReplace } from "@ailly/core/lib/content/content";
+
+export function insert(edit: AillyEdit, after: string, token: string) {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) return;
+  const afterArr = after.split("\n");
+  const start =
+    (isAillyEditReplace(edit) ? edit.start : edit.after) + afterArr.length - 1;
+  editor.edit((builder) => {
+    const line = start;
+    const col = afterArr.at(-1)?.length ?? 0;
+    builder.insert(new vscode.Position(line, col), token);
+  });
+}
